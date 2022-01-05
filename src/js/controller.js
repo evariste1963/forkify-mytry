0fcf584d48280bfce35731fb33e6355a55ec41be
evariste1963 <evariste@hotmail.co.uk>
2022-01-05 22:19:38 +0000

    MVC continued - inc. fractional

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,6 @@
 import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
-// import icons from '../img/icons.svg' // for parcel v1
-import icons from 'url:../img/icons.svg';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -19,141 +18,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-//render spinner --- Parent element is the HTML class element that is the parent (main container) within which the markup sits/fills-out
-const renderSpinner = function (parentEl) {
-  const markup = `
-          <div class="spinner">
-            <svg>
-              <use href="${icons}#icon-loader"></use>
-            </svg>
-          </div>
-  `;
-  parentEl.innerHTML = ''; //clear out HTML container
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-};
-
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1); //get recipe identity from browser HASH -- remove #
     if (!id) return; // break out of function if there is no ID --ie load page first time
-    renderSpinner(recipeContainer);
+    recipeView.renderSpinner();
     //1.loading recipe
 
     await model.loadRecipe(id); //invoke loadRecipe function in model and pass in id
-    const { recipe } = model.state; //get state from model and create recipe object
+
     //2. rendering recipe - create markup from recipe object
-    const markup = `
-        <figure class="recipe__fig">
-        <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
-        <h1 class="recipe__title">
-          <span>${recipe.title}</span>
-        </h1>
-      </figure>
-
-      <div class="recipe__details">
-        <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use href="${icons}#icon-clock"></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--minutes">${
-            recipe.cookingTime
-          }</span>
-          <span class="recipe__info-text">minutes</span>
-        </div>
-        <div class="recipe__info">
-          <svg class="recipe__info-icon">
-            <use href="${icons}#icon-users"></use>
-          </svg>
-          <span class="recipe__info-data recipe__info-data--people">4</span>${
-            recipe.servings
-          }
-          <span class="recipe__info-text">servings</span>
-
-          <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-minus-circle"></use>
-              </svg>
-            </button>
-            <button class="btn--tiny btn--increase-servings">
-              <svg>
-                <use href="${icons}#icon-plus-circle"></use>
-              </svg>
-            </button>
-          </div>
-        </div>
-
-        <div class="recipe__user-generated">
-          <svg>
-            <use href="${icons}#icon-user"></use>
-          </svg>
-        </div>
-        <button class="btn--round">
-          <svg class="">
-            <use href="${icons}#icon-bookmark-fill"></use>
-          </svg>
-        </button>
-      </div>
-
-      <div class="recipe__ingredients">
-        <h2 class="heading--2">Recipe ingredients</h2>
-        <ul class="recipe__ingredient-list">
-        ${recipe.ingredients
-          .map(ing => {
-            return `
-            <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class=${ing.unit}>g</span>
-                ${ing.description}
-              </div>
-            </li>
-          `;
-          })
-          .join('')}
-         
-          <li class="recipe__ingredient">
-            <svg class="recipe__icon">
-              <use href="${icons}#icon-check"></use>
-            </svg>
-            <div class="recipe__quantity">0.5</div>
-            <div class="recipe__description">
-              <span class="recipe__unit">cup</span>
-              ricotta cheese
-            </div>
-          </li>
-        </ul>
-      </div>
-
-      <div class="recipe__directions">
-        <h2 class="heading--2">How to cook it</h2>
-        <p class="recipe__directions-text">
-          This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${
-            recipe.publisher
-          }</span>. Please check out
-          directions at their website.
-        </p>
-        <a
-          class="btn--small recipe__btn"
-          href="${recipe.sourceUrl}"
-          target="_blank"
-        >
-          <span>Directions</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </a>
-      </div>
-      `;
-    recipeContainer.innerHTML = ''; //clear out HTML container
-    recipeContainer.insertAdjacentHTML('afterbegin', markup); //fill container with recipe data
+    recipeView.render(model.state.recipe); //from recipeView render method
   } catch (err) {
     alert(err);
   }
 };
 // load recipe when HASH changes or on load of new page
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
