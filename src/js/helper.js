b8265e8312eda41dc374659ac0b13b276b611bb7
evariste1963 <evariste@hotmail.co.uk>
2022-01-07 21:16:19 +0000

    typo corrocted (❁´◡❁)

--- src/js/helper.js
+++ src/js/helper.js
@@ -14,7 +14,7 @@ export const getJSON = async function (url) {
     const data = await res.json(); //format result using JSON function
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`); //throw error message if no result found
-    return data; // retrun DATA to Model
+    return data; // return DATA to Model
   } catch (err) {
     throw err; //throw error back to Model
   }
