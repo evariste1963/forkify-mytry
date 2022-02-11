ea0015b4e505e0b33d03555b0a92788c4bd09d5e
evariste1963 <evariste@hotmail.co.uk>
2022-02-11 20:06:40 +0000

    lesson 307 complete - Upload new recipe pt1

--- src/js/controller.js
+++ src/js/controller.js
@@ -100,6 +100,11 @@ const controlAddBookmark = function () {
 const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
+
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+  //Upload new recipe data
+};
 //immediately pass controls to Views on startup (subscriber/publisher)
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -108,5 +113,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
