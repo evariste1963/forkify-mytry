49526ac1d4ee416d0eb05a4d9932791913203175
evariste1963 <evariste@hotmail.co.uk>
2022-02-14 19:17:17 +0000

    without fracty etc

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View';
 // import icons from '../img/icons.svg' // for parcel v1
 import icons from 'url:../../img/icons.svg';
-import fracty from 'fracty';
+// import fracty from 'fracty';
 
 //all views aill be classes so that they can be emalgamated into the Parent view class
 class RecipeView extends View {
@@ -129,7 +129,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? fracty(ing.quantity).toString() : ''
+          ing.quantity ? /*fracty*/ ing.quantity.toString() : ''
         }</div>
       
 
